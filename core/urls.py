cdae264b1432de2fbae05327dc3bad7eefa89b3c
KereeceWilliams <kereece.williams@quinnipiac.edu>
2015-10-22 22:56:02 +0000

    Add success page

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
-                       url(r'^message/create/$', MessageCreateView.as_view(), name='message_create')
+                       url(r'^message/create/$', MessageCreateView.as_view(), name='message_create'),
+                       url(r'^success/$', Success.as_view(), name='success'),
                       )
